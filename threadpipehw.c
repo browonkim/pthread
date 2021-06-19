e58c53d9efc52261aca71e2f946a79451c5f020f
browonkim <browonkim@gmail.com>
2021-06-20 01:58:23 +0900

    clear Project

--- threadpipehw.c
+++ threadpipehw.c
@@ -39,8 +39,6 @@ struct stageArg
 };
 typedef struct stageArg stageArg;
 
-int * defectNumbers;
-
 #define ENDMARK -1
 struct autoPartBox *AutoBox;
 pthread_barrier_t barrier;
@@ -51,48 +49,111 @@ long long NPART = 0;
 
 void sendAutoPart(int id, struct autoPart *ap, struct autoPartBox *apBox)
 {
-	printf("SEND:: Stage %d thread waiting on autPartBox %d full \n", id, apBox->bid);
-	printf("SEND:: Stage %d sending autoPart %d to autoPartBox %d\n", id, apBox->lastPart->partNumber, apBox->bid);
-	printf("SEND:: Stage %d signals autoBoxPart %d NOT empty\n", id, apBox->bid);
+	pthread_mutex_lock(&(apBox->mutex));
+	if (apBox->count == apBox->SIZE)
+	{
+		printf("SEND:: Stage %d thread waiting on autPartBox %d full \n", id, apBox->bid);
+		pthread_cond_wait(&(apBox->full), &(apBox->mutex));
+	}
+	if (apBox->count == 0)
+	{
+		(apBox->count) += 1;
+		apBox->firstPart = apBox->lastPart = ap;
+		printf("SEND:: Stage %d sending autoPart %d to autoPartBox %d\n", id, apBox->lastPart->partNumber, apBox->bid);
+		printf("SEND:: Stage %d signals autoBoxPart %d NOT empty\n", id, apBox->bid);
+		pthread_cond_signal(&(apBox->empty));
+	}
+	else
+	{
+		(apBox->count) += 1;
+		apBox->lastPart->next = ap;
+		apBox->lastPart = ap;
+		printf("SEND:: Stage %d sending autoPart %d to autoPartBox %d\n", id, apBox->lastPart->partNumber, apBox->bid);
+	}
+	pthread_mutex_unlock(&(apBox->mutex));
 }
 
 struct autoPart *receiveAutoPart(int id, struct autoPartBox *apBox)
 {
 	autoPart *autoPtr;
-	printf("RECEIVE:: Stage %d waiting on autoPartBox %d empty\n", id, apBox->bid);
+	pthread_mutex_lock(&(apBox->mutex));
+	if (apBox->count == 0)
+	{
+		printf("RECEIVE:: Stage %d waiting on autoPartBox %d empty\n", id, apBox->bid);
+		pthread_cond_wait(&(apBox->empty), &(apBox->mutex));
+	}
+
+	autoPtr = apBox->firstPart;
+	apBox->firstPart = apBox->firstPart->next;
+	if (apBox->count == 1)
+	{
+		apBox->lastPart = NULL;
+	}
 	printf("RECEIVE:: Stage %d receiving autoPart %d from autoPartBox %d\n", id, autoPtr->partNumber, apBox->bid);
-	printf("RECEIVE:: Stage %d signals autoPartBox %d NOT full\n", id, apBox->bid);
+	if (apBox->count == apBox->SIZE)
+	{
+		apBox->count -= 1;
+		printf("RECEIVE:: Stage %d signals autoPartBox %d NOT full\n", id, apBox->bid);
+		pthread_cond_signal(&(apBox->full));
+	}
+	else
+	{
+		apBox->count -= 1;
+	}
+	pthread_mutex_unlock(&(apBox->mutex));
 	return (autoPtr);
 }
 
 // Generate autoParts and put the autoParts into the first autoPartBox
 void *startThread(void *ag)
 {
-	//autoPart 부품을 만들어야함
-	autoPart todo_autoPart;
-	todo_autoPart.next = NULL;
-	todo_autoPart.partNumber = 0;
-	autoPart *autoPtr = &todo_autoPart;
-
-	printf("Start Thread Stage %d sending autoPart %d to autoPartBox %d\n", 0, autoPtr->partNumber, 0);
+	int count;
+	long sum = 0;
+	autoPart *autoPtr = NULL;
+	for (count = 0; count < NPART; count++)
+	{
+		//create new autoPart
+		autoPtr = malloc(sizeof(struct autoPart));
+		autoPtr->next = NULL;
+		autoPtr->partNumber = abs(rand());
+		sum += autoPtr->partNumber;
+		printf("Start Thread Stage %d sending autoPart %d to autoPartBox %d\n", 0, autoPtr->partNumber, 0);
+		sendAutoPart(0, autoPtr, AutoBox);
+	}
+	autoPtr = malloc(sizeof(struct autoPart));
+	autoPtr->next = NULL;
+	autoPtr->partNumber = ENDMARK;
 	printf("Start Thread Stage %d sending ENDMARK to autoPartBox %d\n", 0, 0);
+	sendAutoPart(0, autoPtr, AutoBox);
 
 	pthread_barrier_wait(&barrier);
+	pthread_exit((void *)sum);
 }
 
 // Get autoParts from the last autoPartBox and add all of them
 void *endThread(void *id)
 {
-	int tid = 0;
-	autoPart *autoPtr;
-	autoPart todo_autoPart;
-	todo_autoPart.next = NULL;
-	todo_autoPart.partNumber = 0;
-	autoPtr = &todo_autoPart;
-	printf("End Thread Stage %d receiving autoPart %d from autoPartBox %d\n", tid, autoPtr->partNumber, tid - 1);
-	printf("End Thread Stage %d receiving ENDMARK from autoPartBox %d\n", id, tid - 1);
+	int count;
+	long sum = 0;
+	int tid = (int)((long)id);
+	struct autoPart *autoPtr = NULL;
 
+	while (1)
+	{
+		autoPtr = receiveAutoPart(NSTAGES + 1, AutoBox + NSTAGES);
+		if (autoPtr->partNumber != ENDMARK)
+		{
+			printf("End Thread Stage %d receiving autoPart %d from autoPartBox %d\n", tid, autoPtr->partNumber, tid - 1);
+			sum += (autoPtr->partNumber);
+		}
+		else
+		{
+			printf("End Thread Stage %d receiving ENDMARK from autoPartBox %d\n", tid, tid - 1);
+			break;
+		}
+	}
 	pthread_barrier_wait(&barrier);
+	pthread_exit((void *)sum);
 }
 
 // Check autoParts from the input box and remove faulty parts
@@ -100,102 +161,117 @@ void *endThread(void *id)
 // The faulty part number is a multiple of the stage defect number
 void *stageThread(void *ptr)
 {
+	long sum = 0;
+	stageArg *stArg = (stageArg *)ptr;
+	int defectNumber = stArg->defectNumber;
+	autoPart *autoPtr = NULL;
 
-	stageArg todo_stageArg;
-	todo_stageArg.defectNumber = 0;
-	todo_stageArg.sid = 0;
-	autoPart todo_autoPart;
-	todo_autoPart.next = NULL;
-	todo_autoPart.partNumber = 0;
-	stageArg *stArg = &todo_stageArg;
-	autoPart *autoPtr = &todo_autoPart;
-	printf("Stage %d receiving autoPart %d from autoPartBox %d\n", stArg->sid, autoPtr->partNumber, stArg->sid - 1);
-	printf("Stage %d deleting autoPart %d\n", stArg->sid, autoPtr->partNumber);
-	printf("Stage %d sending autoPart %d to autoPartBox %d\n", stArg->sid, autoPtr->partNumber, stArg->sid);
-	printf("Stage %d receiving ENDMARK from autoPartBox %d\n", stArg->sid, stArg->sid - 1);
-	printf("Stage %d sending ENDMARK to autoPartBox %d\n", stArg->sid, stArg->sid);
-
+	while (1)
+	{
+		autoPtr = receiveAutoPart(stArg->sid, AutoBox + ((stArg->sid) - 1));
+		if (autoPtr->partNumber != ENDMARK)
+		{
+			printf("Stage %d receiving autoPart %d from autoPartBox %d\n", stArg->sid, autoPtr->partNumber, stArg->sid - 1);
+			if (((autoPtr->partNumber) % defectNumber) == 0)
+			{
+				printf("Stage %d deleting autoPart %d\n", stArg->sid, autoPtr->partNumber);
+				sum += autoPtr->partNumber;
+				continue;
+			}
+			else
+			{
+				printf("Stage %d sending autoPart %d to autoPartBox %d\n", stArg->sid, autoPtr->partNumber, stArg->sid);
+				sendAutoPart(stArg->sid, autoPtr, AutoBox + (stArg->sid));
+			}
+		}
+		else
+		{
+			printf("Stage %d receiving ENDMARK from autoPartBox %d\n", stArg->sid, stArg->sid - 1);
+			printf("Stage %d sending ENDMARK to autoPartBox %d\n", stArg->sid, stArg->sid);
+			sendAutoPart(stArg->sid, autoPtr, AutoBox + (stArg->sid));
+			break;
+		}
+	}
 	pthread_barrier_wait(&barrier);
+	pthread_exit((void *)sum);
 }
 
 int main(int argc, char *argv[])
 {
-	//main 인자값들 배정하기
 	if (argc < 5)
 	{
 		printf("Usage: <executable> NSTAGES BOXSIZE NPART defect_numbers\n");
 		exit(1);
 	}
-	
+
 	NSTAGES = atoll(argv[1]);
 	BOXSIZE = atoll(argv[2]);
 	NPART = atoll(argv[3]);
 
-	AutoBox = malloc(sizeof(struct autoPartBox) * (BOXSIZE + 1));
+	AutoBox = malloc(sizeof(struct autoPartBox) * (NSTAGES + 1));
 	if (AutoBox == NULL)
 	{
 		perror(strerror(errno));
 		exit(2);
 	}
 
-	defectNumbers = malloc(sizeof(int) * NSTAGES);
-	if (defectNumbers == NULL)
+	int i, j;
+	j = NSTAGES + 1;
+
+	struct autoPartBox *temp;
+	for (i = 0; i < j; i++)
 	{
-		perror(strerror(errno));
-		exit(2);
-	}
-	
-	int i;
-	for(i=0;i<NSTAGES;i++)
-	{
-		defectNumbers[i] = atoi(argv[i+4]);
+		temp = (AutoBox + i);
+		temp->bid = i;
+		temp->count = 0;
+		temp->firstPart = NULL;
+		temp->lastPart = NULL;
+		pthread_mutex_init(&(temp->mutex), NULL);
+		pthread_cond_init(&(temp->empty), NULL);
+		pthread_cond_init(&(temp->full), NULL);
+		temp->SIZE = BOXSIZE;
 	}
 
-	//배리어 초기화
-	pthread_barrier_init(&barrier, NULL, (NSTAGES+3));
+	pthread_barrier_init(&barrier, NULL, (NSTAGES + 3)); //initialize barrier
 
-	//thread 변수 선언 및 thread 생성
+	//creating threads
 	long int startThreadSum, endThreadSum, stageThreadSum;
-	void *status;
 	startThreadSum = endThreadSum = stageThreadSum = 0;
 	srand(100);
 	pthread_t startTid, endTid;
-	pthread_t * stageTid;
+	pthread_t *stageTid;
 	stageTid = malloc(sizeof(pthread_t) * NSTAGES);
-	int startArg, endArg;
-
-	int * stageArgs;
-	stageArgs = malloc(sizeof(int) * NSTAGES);
-
-	pthread_create(&startTid, NULL, startThread, (void *)&startArg);
-	pthread_create(&endTid, NULL, endThread, (void *)&endArg);
-	
+	pthread_create(&startTid, NULL, startThread, (void *)0);
+	pthread_create(&endTid, NULL, endThread, (void *)(NSTAGES + 1));
+	stageArg *stageArgs = malloc(sizeof(struct stageArg) * NSTAGES);
 	for (i = 0; i < NSTAGES; i++)
 	{
+		stageArgs[i].sid = i + 1;
+		stageArgs[i].defectNumber = atoi(argv[i + 4]);
 		pthread_create((stageTid + i), NULL, stageThread, (void *)(stageArgs + i));
 	}
 
+	long status;
 	pthread_barrier_wait(&barrier);
 	printf("*** Part Sum Information ***\n");
-	pthread_join(startTid, &status);
+	pthread_join(startTid, (void **)&status);
 	printf("startThread sum %ld\n", status);
-	startThreadSum = (long) status;
-	pthread_join(endTid, &status);
+	startThreadSum = (long)status;
+	pthread_join(endTid, (void **)&status);
 	printf("endThread sum %ld\n", status);
-	endThreadSum = (long) status;
+	endThreadSum = (long)status;
 
 	for (i = 0; i < NSTAGES; i++)
 	{
-		pthread_join(stageTid[i], &status);
+		pthread_join(stageTid[i], (void **)&status);
 		stageThreadSum += (long)status;
 		printf("Stage %d sum %ld\n", i, status);
 	}
 
-	//assert(startThreadSum == (endThreadSum + stageThreadSum));
+	assert(startThreadSum == (endThreadSum + stageThreadSum));
 
 	free(stageTid);
 	free(stageArgs);
-	free(defectNumbers);
 	free(AutoBox);
 	pthread_exit(0);
 }
